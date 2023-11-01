7de46124236bd738d5b923f9163838c4a35738cd
bigstar0103 <vista0103@outlook.com>
2023-11-02 06:36:29 +0900

    added: useradd page add

--- pages/userlist/index.tsx
+++ pages/userlist/index.tsx
@@ -2,10 +2,22 @@ import Table from "@/components/Tables/Table";
 import RootLayout from "@/layouts";
 
 import { Metadata } from "next";
+import Link from "next/link";
 const UserList = () => {
   return (
     <>
       <div className="flex flex-col gap-10 mt-13">
+        <div className="flex justify-end">
+          <button
+            className="flex justify-center rounded bg-primary py-2 px-6 font-medium w-30 text-gray hover:bg-opacity-95"
+            type="submit"
+          // onClick={onsubmit}
+          >
+            <Link href="/useradd">
+              Add User
+            </Link>
+          </button>
+        </div>
         <Table />
       </div>
     </>
